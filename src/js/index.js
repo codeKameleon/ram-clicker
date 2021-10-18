c9f93aba67c4025e2e4dd2e05964e6d36af36664
axel <axel@pop-os.localdomain>
2021-10-18 14:48:40 +0200

    Implementing bootScore logic (in progress)

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,8 @@
 import { increaseScore } from './increaseScore'; 
+import { intervalTrigger } from './boostScore';
+
 const ramClicker = document.querySelector('#ramClick')
+const boostBtn = document.querySelector('.boost')
 
 ramClicker.addEventListener('click', increaseScore)
+boostBtn.addEventListener('click', intervalTrigger)
