2ae80fa53d72d70d7f650bc51c43f416cd54667d
axel <axel@pop-os.localdomain>
2021-10-18 16:41:01 +0200

    refactoring + points per click update (in progress)

--- src/js/index.js
+++ src/js/index.js
@@ -2,25 +2,33 @@ import { increaseScore } from "./increaseScore";
 import { intervalTrigger } from "./boostScore";
 import { multiplier } from "./multiplier";
 
+// VARIABLES
 let score = 0;
-export let pointPerClick = 1;
-const multiplierX2 = document.querySelector("#x2");
+let pointsPerClick = 1;
 
+// DOM ELEMENTS
 const ramClicker = document.querySelector("#ramClick");
+const multiplierX2Btn = document.querySelector("#x2");
 const boostBtn = document.querySelector(".boost");
+const scoreDisplay = document.querySelector(".score-display");
 
-ramClicker.addEventListener("click", increaseScore);
-boostBtn.addEventListener("click", intervalTrigger);
-
+// EVENT LISTENERS
 ramClicker.addEventListener("click", () => {
+  score = increaseScore(score, pointsPerClick, scoreDisplay)
+
   if (score >= 10) {
-    multiplierX2.removeAttribute("disabled");
+    multiplierX2Btn.removeAttribute("disabled");
+    multiplierX2Btn.classList.remove('upgrade-inactive')
+    multiplierX2Btn.classList.add('upgrade-active')
   }
-  score = increaseScore(score);
 });
 
-export let multiplierBtn = document.querySelector(".multiplier");
-multiplierBtn.innerText = "10 points for 2 points per click";
-multiplierBtn.addEventListener("click", () => {
-  score = multiplier(score);
-});
+/*Mulitplier*/
+multiplierX2Btn.addEventListener("click", () => {
+  score = multiplier(score, pointsPerClick, scoreDisplay);
+  pointsPerClick = multiplier(score, multiplier[1], scoreDisplay)
+}); 
+
+/*Auto Clicker*/ 
+/*Boost*/ 
+boostBtn.addEventListener("click", intervalTrigger);
