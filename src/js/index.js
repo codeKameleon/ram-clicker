54214431292b00f178d5c34956f134adae6d22f4
YoussefAkanni <89466060+YoussefAkanni@users.noreply.github.com>
2021-10-19 10:09:12 +0200

    add mut-ltiplier x2 x5

--- src/js/index.js
+++ src/js/index.js
@@ -10,6 +10,7 @@ let pointsPerClick = 1;
 // DOM ELEMENTS
 const ramClicker = document.querySelector("#ramClick");
 const multiplierX2Btn = document.querySelector("#x2");
+const multiplierX5Btn = document.querySelector("#x5");
 const boostBtn = document.querySelector(".boost");
 const scoreDisplay = document.querySelector(".score-display");
 
@@ -22,19 +23,32 @@ ramClicker.addEventListener("click", () => {
     multiplierX2Btn.classList.remove("upgrade-inactive");
     multiplierX2Btn.classList.add("upgrade-active");
   }
+  if (score >= 50) {
+    multiplierX5Btn.removeAttribute("disabled");
+    multiplierX5Btn.classList.remove("upgrade-inactive");
+    multiplierX5Btn.classList.add("upgrade-active");
+  }
+});
+
+/*Mulitplier*/
+// x2
+multiplierX2Btn.addEventListener("click", () => {
+  let multiplayerArray = multiplier(score, pointsPerClick, scoreDisplay);
+  score = multiplayerArray[0];
+  pointsPerClick = multiplayerArray[1];
+});
+
+// x5
+multiplierX5Btn.addEventListener("click", () => {
+  let multiplayerArray = multiplier(score, pointsPerClick, scoreDisplay);
+  score = multiplayerArray[0];
+  pointsPerClick = multiplayerArray[1];
 });
 
 // Autoclicker
 const autoClickerButton = document.querySelector(".auto-clicker");
 autoClickerButton.addEventListener("click", (e) => {
   score = autoClicker(score);
-});
-
-/*Mulitplier*/
-multiplierX2Btn.addEventListener("click", () => {
-  let multiplayerArray = multiplier(score, pointsPerClick, scoreDisplay);
-  score = multiplayerArray[0];
-  pointsPerClick = multiplayerArray[0];
 });
 
 /*Boost*/
--- src/js/multiplier.js
+++ src/js/multiplier.js
@@ -1,17 +1,19 @@
+const multiplierX5Div = document.querySelector(".multiplierX5-upgrade");
+const multiplierX2Div = document.querySelector(".multiplierX2-upgrade");
+
 export function multiplier(score, pointsPerClick, scoreDisplay) {
+  if (score >= 50) {
+    score -= 50;
+    pointsPerClick = 5;
+    scoreDisplay.innerText = score;
+    multiplierX5Div.classList.add("inactive");
+  }
+
   if (score >= 10) {
     score -= 10;
-    pointsPerClick *= 2;
+    pointsPerClick = 2;
     scoreDisplay.innerText = score;
-    console.log("points per click", pointsPerClick);
-
-    // if (score >= 50) {
-    //   score -= 50;
-    //   pointPerClick = 4;
-    //   multiplierBtn.innerText =
-    //     "100 points for " + PointPerClick + " points per click";
-    //   scoreDisplay.innerText = score;
-    // }
+    multiplierX2Div.classList.add("inactive");
   }
 
   return [score, pointsPerClick];
