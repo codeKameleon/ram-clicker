7fb0710255372dd1d07558613a630dadd3de149b
YoussefAkanni <89466060+YoussefAkanni@users.noreply.github.com>
2021-10-19 11:02:50 +0200

    add multiplier x10

--- src/js/index.js
+++ src/js/index.js
@@ -11,6 +11,7 @@ let pointsPerClick = 1;
 const ramClicker = document.querySelector("#ramClick");
 const multiplierX2Btn = document.querySelector("#x2");
 const multiplierX5Btn = document.querySelector("#x5");
+const multiplierX10Btn = document.querySelector("#x10");
 const boostBtn = document.querySelector(".boost");
 const scoreDisplay = document.querySelector(".score-display");
 
@@ -28,6 +29,11 @@ ramClicker.addEventListener("click", () => {
     multiplierX5Btn.classList.remove("upgrade-inactive");
     multiplierX5Btn.classList.add("upgrade-active");
   }
+  if (score >= 300) {
+    multiplierX10Btn.removeAttribute("disabled");
+    multiplierX10Btn.classList.remove("upgrade-inactive");
+    multiplierX10Btn.classList.add("upgrade-active");
+  }
 });
 
 /*Mulitplier*/
@@ -40,6 +46,12 @@ multiplierX2Btn.addEventListener("click", () => {
 
 // x5
 multiplierX5Btn.addEventListener("click", () => {
+  let multiplayerArray = multiplier(score, pointsPerClick, scoreDisplay);
+  score = multiplayerArray[0];
+  pointsPerClick = multiplayerArray[1];
+});
+// x10
+multiplierX10Btn.addEventListener("click", () => {
   let multiplayerArray = multiplier(score, pointsPerClick, scoreDisplay);
   score = multiplayerArray[0];
   pointsPerClick = multiplayerArray[1];
--- src/js/multiplier.js
+++ src/js/multiplier.js
@@ -1,15 +1,19 @@
+const multiplierX10Div = document.querySelector(".multiplierX10-upgrade");
 const multiplierX5Div = document.querySelector(".multiplierX5-upgrade");
 const multiplierX2Div = document.querySelector(".multiplierX2-upgrade");
 
 export function multiplier(score, pointsPerClick, scoreDisplay) {
-  if (score >= 50) {
+  if (score >= 300) {
+    score -= 300;
+    pointsPerClick = 10;
+    scoreDisplay.innerText = score;
+    multiplierX10Div.classList.add("inactive");
+  } else if (score >= 50) {
     score -= 50;
     pointsPerClick = 5;
     scoreDisplay.innerText = score;
     multiplierX5Div.classList.add("inactive");
-  }
-
-  if (score >= 10) {
+  } else if (score >= 10) {
     score -= 10;
     pointsPerClick = 2;
     scoreDisplay.innerText = score;
