2edbba020001c5472eafd642b6a89fd9348fba42
axel <axel@pop-os.localdomain>
2021-10-19 11:24:18 +0200

    refactoring

--- src/js/multiplier.js
+++ src/js/multiplier.js
@@ -1,3 +1,4 @@
+const upgradesDiv = document.querySelector('.upgrades')
 const multiplierX10Div = document.querySelector(".multiplierX10-upgrade");
 const multiplierX5Div = document.querySelector(".multiplierX5-upgrade");
 const multiplierX2Div = document.querySelector(".multiplierX2-upgrade");
@@ -7,17 +8,17 @@ export function multiplier(score, pointsPerClick, scoreDisplay) {
     score -= 300;
     pointsPerClick = 10;
     scoreDisplay.innerText = score;
-    multiplierX10Div.classList.add("inactive");
+    upgradesDiv.removeChild(multiplierX10Div);
   } else if (score >= 50) {
     score -= 50;
     pointsPerClick = 5;
     scoreDisplay.innerText = score;
-    multiplierX5Div.classList.add("inactive");
+    upgradesDiv.removeChild(multiplierX5Div);
   } else if (score >= 10) {
     score -= 10;
     pointsPerClick = 2;
     scoreDisplay.innerText = score;
-    multiplierX2Div.classList.add("inactive");
+    upgradesDiv.removeChild(multiplierX2Div);
   }
 
   return [score, pointsPerClick];
