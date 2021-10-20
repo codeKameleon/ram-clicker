cba29c49cff18b7991e131091c9be97e90311618
axel <axel@pop-os.localdomain>
2021-10-20 11:29:22 +0200

    debugging autoClicker function

--- src/js/activateBonus.js
+++ src/js/activateBonus.js
@@ -1,5 +1,40 @@
-export const activateBonus = bonusBtn => {
+import { 
+    multiplierX2Btn, 
+    multiplierX5Btn,
+    multiplierX10Btn,
+    autoClickerBtn,
+    autoClickerOverkillBtn,
+    boostBtn,
+  } from "./domElements"
+ 
+ const addStyleToBonusBtn = bonusBtn => {
     bonusBtn.removeAttribute("disabled")
     bonusBtn.classList.remove('upgrade-inactive')
     bonusBtn.classList.add("upgrade-active")
+}
+
+export const activateBonus = score => {
+    if (score >= 1000) {
+        addStyleToBonusBtn(boostBtn)
+    }
+
+    if (score >= 500) {
+        addStyleToBonusBtn(autoClickerOverkillBtn)
+    }
+
+    if (score >= 1) {
+        addStyleToBonusBtn(autoClickerBtn)
+    }
+
+    if (score >= 100) {
+        addStyleToBonusBtn(multiplierX10Btn)
+    }
+
+    if (score >= 50) {
+        addStyleToBonusBtn(multiplierX5Btn)
+    }
+
+    if (score >= 20) {
+        addStyleToBonusBtn(multiplierX2Btn)
+    }    
 }
