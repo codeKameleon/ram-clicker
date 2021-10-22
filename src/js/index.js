106e8ba005ad3f82d248a3b6b6a6fc20a7e156e3
Dyl-Richardson <dylanrichardson1606@gmail.com>
2021-10-22 16:06:14 +0200

    update

--- src/js/index.js
+++ src/js/index.js
@@ -2,14 +2,18 @@ import { shop } from "./shopList";
 
 let ram = 0
 let ramPerSecond = 0
+let pointPerClick = 1
 
 const ramLocation = document.querySelector(".score-display")
 ramLocation.innerText = ram
 
-let i = 0
+const ramClick = document.querySelector(".ram-click")
+ramClick.addEventListener("click" , e => {
+ ramLocation.innerText = ram += 100
+})
 
-for (const upgrade of shop) {
-  console.log(i);
+
+for (let i = 0; i < shop.length; i++) {
   const shopLocation = document.querySelector(".upgrades")
 
   const div = document.createElement("div")
@@ -17,11 +21,12 @@ for (const upgrade of shop) {
   const button = document.createElement("button")
   button.innerText = shop[i].name
   button.className = "upgradeButton"
-    button.addEventListener("click", e => {
-      if (ram >= shop[i].cost) {
-        ram -= shop[i].cost
-      }
-    })
+  button.addEventListener("click" , e => {
+    if (ram >= shop[i].cost) {
+      ramLocation.innerText = ram -= shop[i].cost
+      ramPerSecond += shop[i].ramPS
+    }
+  })
   div.appendChild(button)
 
   const description = document.createElement("p")
@@ -31,11 +36,12 @@ for (const upgrade of shop) {
   const cost = document.createElement("p")
   cost.innerText = "Cost : " + shop[i].cost + " ram"
   div.appendChild(cost)
-  
-  ramPerSecond += shop[i].ramPS
-  
+
   shopLocation.appendChild(div)
-  i++
+  
 }
 
-const upgradeButton = document.querySelector(".upgradeButton")[0]
+setInterval(() => {
+  ram += ramPerSecond
+  ramLocation.innerText = ram 
+}, 1000)
